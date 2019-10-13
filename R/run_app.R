28432a43a7ae5a6ba7213ab9636eb06c2ce2d7e2
Thiloshon <thiloshon@gmail.com>
2019-10-13 21:56:56 +0530

    adding new graphics

--- R/run_app.R
+++ R/run_app.R
@@ -3,6 +3,6 @@
 #' @export
 #' @importFrom shiny shinyApp
 #' @importFrom golem with_golem_options
-run_app <- function() {
+bdchecks_app <- function() {
     shiny::runApp(system.file("app", package = "bdchecks.app"), launch.browser = TRUE, port = 876)
 }
